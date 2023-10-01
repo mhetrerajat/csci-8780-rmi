8a0c9c9e889364cb22959d2ba05d70d4aad878ff
Rajat Mhetre <mhetrerajat@gmail.com>
2023-09-30 23:47:06 -0400

    fix(client): fix the cli crashing bug

--- app/src/main/java/rmiproject/RMIClientMain.java
+++ app/src/main/java/rmiproject/RMIClientMain.java
@@ -11,51 +11,47 @@ public class RMIClientMain {
 
     private static final Logger logger = Logger.getLogger(RMIClientMain.class.getName());
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RemoteException, NotBoundException {
 
         // Get configuration values using ConfigReader
         String serverHost = ConfigReader.getServerHost();
         Integer serverPort = ConfigReader.getServerPort();
         String bindName = ConfigReader.getRemoteObjectBindName();
 
-        try {
-            // Get the registry
-            Registry registry = LocateRegistry.getRegistry(serverHost, serverPort);
+        // Get the registry
+        Registry registry = LocateRegistry.getRegistry(serverHost, serverPort);
 
-            // Lookup the remote object from the registry
-            RMIInterface remoteObject = (RMIInterface) registry.lookup(bindName);
+        // Lookup the remote object from the registry
+        RMIInterface remoteObject = (RMIInterface) registry.lookup(bindName);
 
-            // Create a Scanner for user input
-            Scanner scanner = new Scanner(System.in);
+        // Create a Scanner for user input
+        Scanner scanner = new Scanner(System.in);
+        int choice;
 
-            // Interactive CLI loop
-            while (true) {
-                System.out.println("1. Say Hello");
-                System.out.println("2. Exit");
-                System.out.print("Enter your choice: ");
-                int choice = scanner.nextInt();
+        // Interactive CLI loop
+        do {
+            System.out.println("1. Say Hello");
+            System.out.println("9. Exit");
+            System.out.print("Enter your choice: ");
 
-                switch (choice) {
-                    case 1:
-                        String result = remoteObject.sayHello();
-                        System.out.println("Remote method result: " + result);
-                        break;
-                    case 2:
-                        System.out.println("Exiting...");
-                        scanner.close();
-                        System.exit(0);
-                        break;
-                    default:
-                        System.out.println("Invalid choice. Please try again.");
-                        break;
-                }
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    String result = remoteObject.sayHello();
+                    System.out.println("Remote method result: " + result);
+                    break;
+                case 9:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
             }
+        } while (choice != 9);
 
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        } catch (NotBoundException e) {
-            e.printStackTrace();
-        }
+        scanner.close();
+
     }
 
 }
