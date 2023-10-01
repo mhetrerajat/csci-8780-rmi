ce21800e31e9ba8a91af5491412777fd4247c68b
Rajat Mhetre <mhetrerajat@gmail.com>
2023-09-30 21:33:05 -0400

    feat(config): add support to read config from resource

--- app/src/main/java/rmiproject/RMIInterface.java
+++ app/src/main/java/rmiproject/RMIInterface.java
@@ -5,5 +5,4 @@ import java.rmi.RemoteException;
 
 public interface RMIInterface extends Remote {
     String sayHello() throws RemoteException;
-    String sendMessage(String message) throws RemoteException;
 }
