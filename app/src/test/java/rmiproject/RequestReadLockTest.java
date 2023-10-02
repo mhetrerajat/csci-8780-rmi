d14a83e4fe18cddd54a2f255e1b5fed573dd6d0f
Rajat Mhetre <mhetrerajat@gmail.com>
2023-10-02 15:17:49 -0400

    refactor(test): remove unnecessary imports

--- app/src/test/java/rmiproject/RequestReadLockTest.java
+++ app/src/test/java/rmiproject/RequestReadLockTest.java
@@ -6,13 +6,9 @@ package rmiproject;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.rmi.RemoteException;
-import java.util.ArrayList;
 import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.BeforeEach;
